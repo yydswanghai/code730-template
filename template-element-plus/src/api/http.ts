c46bbd25fe46a95df89fd85476d0e82400114c7b
wanghai <2275981101@qq.com>
2024-05-17 13:30:45 +0800

    fix: update

--- template-element-plus/src/api/http.ts
+++ template-element-plus/src/api/http.ts
@@ -1,87 +1,78 @@
 import axios from 'axios'
-import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
-import Qs from 'qs'
+import type { AxiosInstance, InternalAxiosRequestConfig, AxiosRequestConfig, AxiosResponse } from 'axios'
+import qs from 'qs'
 import { statusCodeEnum } from '@/enums/statusCodeEnum'
 
 // 加载环境变量
 const { VITE_PROXY_PATH } = import.meta.env;
+const HAS_TOKEN = false;
 
-export interface Result {
-    code: number
-    msg: string
-}
-
-export interface ResultData<T = any> extends Result {
-    data?: T
+export interface ResultData<T = any> {
+    code: number;
+    msg: string;
+    data?: T;
+    [prop: string]: any;
 }
 
 class RequestHttp {
     service: AxiosInstance;
     constructor(){
-        /**
-         * 创建 axios 实例
-         */
+        /* 创建 axios 实例 */
         this.service = axios.create({
             baseURL: VITE_PROXY_PATH,
             timeout: 20000,
             withCredentials: true// 跨域时候允许携带凭证
         })
-        /**
-         * 请求拦截
-         */
-        this.service.interceptors.request.use((config) => {
-            let data = config.data;
-            if(config.method === 'post'){
-                // axios 自动修改请求头里的 content-type 为 application/x-www-form-urlencoded
-                data = Qs.stringify(data);
+        this.interceptorsRequest();
+        this.interceptorsResponse();
+    }
+    /* 请求拦截 */
+    private interceptorsRequest (){
+        this.service.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+            if (HAS_TOKEN) {// 发送请求的时候，如果有token，需要附带到请求头中
+                let headers = config.headers
+                headers.authorization = `bearer ${localStorage.getItem("token")}`;
+                config.headers = headers;
             }
-            // todo1. 发送请求的时候，如果有token，需要附带到请求头中
-            // const token = localStorage.getItem("token");
-            // if(token){
-            //     headers.authorization = `bearer ${token}`
-            // }
-            return {
-                ...config,
-                data
+            if (config.method === 'post' && config.data) {
+                config.data = qs.stringify(config.data);// 'application/x-www-form-urlencoded;'
             }
-        },error => {
+            return config;
+        }, (error: any) => {
             return Promise.reject(error);
-        })
-        /**
-         * 响应拦截
-         */
+        });
+    }
+    /* 响应拦截 */
+    private interceptorsResponse (){
         this.service.interceptors.response.use((resp: AxiosResponse) => {
-            // todo2. 响应的时候，如果有token，保存token到客户端
-            if (resp.headers.authorization) {
-                // localStorage.setItem("token", resp.headers.authorization);
+            if (HAS_TOKEN && resp.headers.authorization) {// 响应的时候，如果有token，保存token到客户端
+                localStorage.setItem("token", resp.headers.authorization);
             }
             return resp.data;
-        },error => {
-             // todo3. 响应的时候，如果响应的消息码是403（没有token，token失效），在本地删除token
-            if (error.response.status === statusCodeEnum.overdue) {
-                // localStorage.removeItem("token");
+        }, (error: any) => {
+            if (HAS_TOKEN && error.response.status === statusCodeEnum.overdue) {// 响应的时候，如果响应的消息码是403（没有token，token失效），在本地删除token
+                localStorage.removeItem("token");
                 const message = Reflect.get(window, '$message') || null
                 message && message.error(error.response.data?.msg);
             }
             return Promise.reject(error);
-        })
+        });
     }
-
     // 常用方法封装
-    request(config: AxiosRequestConfig<any>): Promise<ResultData<any>>{
-        return this.service.request(config)
+    request<T = any>(config: AxiosRequestConfig): Promise<ResultData<T>>{
+        return this.service.request<any, ResultData<T>>(config);
     }
     get<T = any>(url: string, config?: AxiosRequestConfig): Promise<ResultData<T>> {
-        return this.service.get(url, config);
+        return this.service.get<any, ResultData<T>>(url, config);
     }
     post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<ResultData<T>> {
-        return this.service.post(url, data, config);
+        return this.service.post<any, ResultData<T>>(url, data, config);
     }
     put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<ResultData<T>> {
-        return this.service.put(url, data, config);
+        return this.service.put<any, ResultData<T>>(url, data, config);
     }
     delete<T = any>(url: string, config?: AxiosRequestConfig): Promise<ResultData<T>> {
-        return this.service.delete(url, config);
+        return this.service.delete<any, ResultData<T>>(url, config);
     }
 }
 
